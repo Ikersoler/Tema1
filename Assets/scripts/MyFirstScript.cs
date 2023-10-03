8a0d38ee5a10e9add647b788c92c0c7e994331b2
Ikersoler <ikersonic@gmail.com>
2023-10-03 12:48:06 +0200

    csharp

---
+++ Assets/scripts/MyFirstScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+
+    /*comentario multilinea 
+     * -
+     * -
+     */
+
+   [SerializeField] private int age = 18; // valor predeterminado =0;
+
+    [SerializeField] private float speed = 1.5f; // valor predeterminado =0.0f;
+    //poner una f final al finarl de una variable float
+
+    [SerializeField] private bool isGameOver = false; // valor predeterminado = false
+
+    [SerializeField] private string userName = "federico"; // valor predeterminado = ""
+
+
+}
